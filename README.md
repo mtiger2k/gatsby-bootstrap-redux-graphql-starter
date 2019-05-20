601f6002a6d26382e2b0017c1643d84d8e2b7284
shaoqintian <454412129@qq.com>
2019-05-20 16:21:25 +0800

    add bootstrap support

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
   </a>
 </p>
 <h1 align="center">
-  Gatsby's hello-world starter
+  Gatsby's bootstrap redux graphql starter
 </h1>
 
 Kick off your project with this hello-world boilerplate. This starter ships with the main Gatsby configuration files you might need to get up and running blazing fast with the blazing fast app generator for React.
@@ -16,11 +16,11 @@ _Have another more specific idea? You may want to check out our vibrant collecti
 
 1.  **Create a Gatsby site.**
 
-    Use the Gatsby CLI to create a new site, specifying the hello-world starter.
+    Use the Gatsby CLI to create a new site, specifying bootstrap-redux-graphql starter.
 
     ```sh
     # create a new Gatsby site using the hello-world starter
-    gatsby new my-hello-world-starter https://github.com/gatsbyjs/gatsby-starter-hello-world
+    gatsby new bootstrap-redux-graphql-starter https://github.com/mtiger2k/gatsby-bootstrap-redux-graphql-starter
     ```
 
 1.  **Start developing.**
@@ -28,7 +28,7 @@ _Have another more specific idea? You may want to check out our vibrant collecti
     Navigate into your new site’s directory and start it up.
 
     ```sh
-    cd my-hello-world-starter/
+    cd bootstrap-redux-graphql-starter/
     gatsby develop
     ```
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,35 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  pathPrefix: "/gatsby-bootstrap-redux-graphql-starter",
+  siteMetadata: {
+    title: `Gatsby React Bootstrap Redux Graphql Starter`,
+    description: `A starter that includes react-bootstrap and react-icons, along with SASS compilation, with redux and graphq support.`,
+    author: `Scott Tian`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-bootstrap-redux-graphql-starter`,
+        short_name: `react-bootstrap-redux-graphql`,
+        start_url: `/`,
+        background_color: `#20232a`,
+        theme_color: `#20232a`,
+        display: `minimal-ui`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
